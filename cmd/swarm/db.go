65d3553a4ca333c66cd20f11aad93bc6076b70db
G-zhanghao <zhanghao@genaro.network>
2018-09-29 17:45:26 +0800

    update pkg

--- cmd/swarm/db.go
+++ cmd/swarm/db.go
@@ -26,7 +26,6 @@ import (
 	"github.com/GenaroNetwork/Genaro-Core/log"
 	"github.com/GenaroNetwork/Genaro-Core/swarm/storage"
 	"gopkg.in/urfave/cli.v1"
-	"github.com/mkideal/cli"
 )
 
 func dbExport(ctx *cli.Context) {
