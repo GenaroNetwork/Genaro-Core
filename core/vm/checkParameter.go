e5a794239d3a37ea69550faa3bb3dea2a90190ca
eason <eason@genaro.network>
2019-01-24 17:09:36 +0800

    fix bug[bucket supplement][don't read local time when check param]

--- core/vm/checkParameter.go
+++ core/vm/checkParameter.go
@@ -260,10 +260,12 @@ func CheckBucketSupplement(s types.SpecialTxInput, state StateDB, genaroConfig *
 		return errors.New("param [ msg ] missing or can't be null")
 	}
 
-	_, err := strconv.Atoi(s.Message)
+	timeInt, err := strconv.Atoi(s.Message)
 	if err != nil {
 		return errors.New("param [ msg ] is not timestamp")
 	}
+
+	txTime := time.Unix(int64(timeInt), 0)
 
 	adress := common.HexToAddress(s.Address)
 	if isSpecialAddress(adress, genaroConfig.OptionTxMemorySize) {
@@ -282,7 +284,7 @@ func CheckBucketSupplement(s types.SpecialTxInput, state StateDB, genaroConfig *
 
 	if b, ok := buckets[s.BucketID]; ok {
 		bucketInDb := b.(types.BucketPropertie)
-		if bucketInDb.TimeEnd <= uint64(time.Now().Unix()) {
+		if bucketInDb.TimeEnd <= uint64(txTime.Unix()) {
 			return errors.New("the bucket corresponding to the bucketId has has been expired")
 		}
 	} else {
